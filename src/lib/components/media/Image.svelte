a65b6aebfb88ff52964b205420b762f2e7dda238
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-12-23 16:42:41 +0100

    Implement Image component

---
+++ src/lib/components/media/Image.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { loadMedia } from "$lib/utils/media";
+
+    export let src: string;
+    export let storeOffline = false;
+
+    let dataUrl = "";
+
+    onMount(async function() {
+        dataUrl = await loadMedia(src, storeOffline);
+    });
+</script>
+
+{#if dataUrl}
+    <img src={dataUrl}>
+{:else}
+    <span>Loading..</span>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import { loadMedia } from "$lib/utils/media";
+    import Image from "$lib/components/media/Image.svelte";
 
-    let src = "";
-
-    async function loadImage() {
-        src = await loadMedia("https://us-a.tapas.io/c/d0/f20e5adc-1dd7-4f36-b614-95fc45a6a4d0.png", true);
-    }
+    let showImage = false;
 </script>
 
 <h1>Hello, world!</h1>
 
-<button on:click={loadImage}>Load image</button>
-<img src={src}>
+<input type="checkbox" bind:checked={showImage}> <br>
+
+{#if showImage}
+    <Image src={"https://us-a.tapas.io/c/d0/f20e5adc-1dd7-4f36-b614-95fc45a6a4d0.png"} storeOffline/>
+{/if}
