e079a7dadb1f1f266e7d04898851f88e961db680
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-12-27 17:44:23 +0100

    Add listener for device back button

--- src/lib/utils/page-history/index.ts
+++ src/lib/utils/page-history/index.ts
@@ -1,3 +1,4 @@
+import { App as CapacitorApp } from "@capacitor/app";
 import { pageStateStore } from "../../../stores";
 import { get } from "svelte/store";
 import { browser } from "$app/environment";
@@ -10,6 +11,10 @@ export function commitToHistory() {
 
 export function goBack() {
     window.history.back();
+
+    if (window.location.hash = "#") {
+        CapacitorApp.exitApp();
+    }
 }
 
 export function goForward() {
@@ -23,14 +28,14 @@ if (browser) {
         if (hash != encodeURIComponent(JSON.stringify(get(pageStateStore)))) {
             pageStateStore.set(JSON.parse(decodeURIComponent(hash)));
 
-            /*
             // Quit the appliaction if the user navigates back to these pages
-            if (get(pageStateStore).currentPage == "loading" || get(pageStateStore).currentPage == "introduction") {
+            if (get(pageStateStore).currentPage == "loading") {
                 CapacitorApp.exitApp();
                 console.log("Quitting!");
                 return;
             }
-            */
         }
     });
+
+    CapacitorApp.addListener("backButton", goBack);
 }
