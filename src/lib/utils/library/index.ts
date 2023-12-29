537cef9be040d5ce22e92dba2d0c0a908bb59849
TriLinder <82846064+TriLinder@users.noreply.github.com>
2023-12-29 12:27:42 +0100

    Delete series' media assets when removed from library

--- src/lib/utils/library/index.ts
+++ src/lib/utils/library/index.ts
@@ -1,5 +1,6 @@
 import { libraryStore } from "../../../stores";
 import { get } from "svelte/store";
+import { deleteMediaFromStroage } from "../media";
 import type { Series } from "$lib/types";
 
 export function addSeriesToLibrary(series: Series) {
@@ -12,4 +13,16 @@ export function removeSeriesFromLibrary(series: Series) {
     const libraryStoreValue = get(libraryStore);
     delete libraryStoreValue.series[series.id];
     libraryStore.set(libraryStoreValue);
+
+    // Now delete the series' media assets
+    deleteMediaFromStroage(series.coverImageUrl);
+    deleteMediaFromStroage(series.thumbnailUrl);
+    
+    for (const creator of series.creators) {
+        deleteMediaFromStroage(creator.profilePictureUrl);
+    }
+
+    for (const episode of series.episodes) {
+        deleteMediaFromStroage(episode.thumbnailUrl);
+    }
 }
