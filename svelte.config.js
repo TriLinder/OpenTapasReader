2ddf18ac519124ef2083def102395548fbc4465f
TriLinder <82846064+TriLinder@users.noreply.github.com>
2024-01-09 17:31:25 +0100

    Use @sveltejs/adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@ptkdev/sveltekit-cordova-adapter';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,8 +12,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			strict: false,
-			platform: "cordova"
+			strict: false
 		})
 	}
 };
